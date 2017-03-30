cfdc4ca2ae7b50095a495d3ea797990b8d5976ef
joseph hoffman <Joe@josephs-MacBook-Pro-2.local>
2017-03-30 16:44:51 -0600

    made change to examples.jsc

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 // See About.jsx for an explination of this
 var Examples = (props) => {
   return (
-    <h3>Examples Component</h3>
+    <div>
+      <h3>Examples Component</h3>
+      <p></p>
+    </div>
   )
 };
 
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 
 1. if bundle.js is not generated run "webpack" from the terminal
 2. run "node server.js" in the command line
+# ReactWeather
