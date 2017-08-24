b02122c55fd3c5c63721cd3e9f58af1af246d2bd
joseph hoffman <Joe@josephs-MacBook-Pro-2.local>
2017-08-24 09:57:35 -0600

    updated about page with styling

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,20 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
+      <h1 className="text-center">About</h1>
       <p>Welcome to the about page!</p>
+      <p>
+        Here are some of teh tolls I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react"> React </a> - This was the javascript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - Use for weather api
+        </li>
+
+      </ul>
     </div>
   )
 };
